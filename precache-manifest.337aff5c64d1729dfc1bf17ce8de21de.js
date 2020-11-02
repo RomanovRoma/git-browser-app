7d1342875b6b831a5adbe1ead6c164af49eeedd9
RomanovRoma <romanian.reget@gmail.com>
2020-11-02 18:03:57 +0300

    Updates

---
+++ precache-manifest.337aff5c64d1729dfc1bf17ce8de21de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26303e4ad739eca02f0ec7d0ca54aef3",
+    "url": "/git-browser-app/index.html"
+  },
+  {
+    "revision": "22308b9c75063cc450a9",
+    "url": "/git-browser-app/static/css/main.24c1fc60.chunk.css"
+  },
+  {
+    "revision": "361f4dbb1afe66b1f01a",
+    "url": "/git-browser-app/static/js/2.0d3f5d07.chunk.js"
+  },
+  {
+    "revision": "c2e52282d033fca86fa91aac36af9388",
+    "url": "/git-browser-app/static/js/2.0d3f5d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22308b9c75063cc450a9",
+    "url": "/git-browser-app/static/js/main.2efcf1c8.chunk.js"
+  },
+  {
+    "revision": "4c7fb719809c474463f8",
+    "url": "/git-browser-app/static/js/runtime-main.193e7178.js"
+  }
+]);
